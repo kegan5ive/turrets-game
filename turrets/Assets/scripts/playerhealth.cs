32a68bd13448e9d18d145b1eed7556ed9380aa2c
kegan5ive <kfogarty2502@gmail.com>
2017-10-30 14:42:48 -0400

    1st upload

---
+++ turrets/Assets/scripts/playerhealth.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerhealth : MonoBehaviour {
+
+	int health = 3;
+	//when hit by bullet health -1, bullet disipears
+	//when health = 0 u lose
+		
+
+}
